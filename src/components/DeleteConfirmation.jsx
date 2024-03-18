51ec4dc8a8cb03e4a10503fca2d52165568de720
SonjaLu <s_lueg@yahoo.com>
2024-03-18 21:10:37 +0100

    adding Progressbar

--- src/components/DeleteConfirmation.jsx
+++ src/components/DeleteConfirmation.jsx
@@ -1,10 +1,19 @@
 import { useEffect } from "react";  
+import ProgressBar from './ProgressBar.jsx';
+
+const TIMER = 3000;
 
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
+
+
+
+
+
+
   useEffect(() => {
     const timer = setTimeout(() => {
       onConfirm();
-    }, 3000);
+    }, TIMER);
 
     return () => {
       clearTimeout(timer);
@@ -24,6 +33,7 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
           Yes
         </button>
       </div>
+      <ProgressBar timer={TIMER}/>
     </div>
   );
 }
