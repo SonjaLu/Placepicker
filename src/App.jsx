40afab1683e3b6589e527b3bf75b3e1992f7ca43
SonjaLu <s_lueg@yahoo.com>
2024-03-18 20:31:10 +0100

    setting timer

--- src/App.jsx
+++ src/App.jsx
@@ -11,10 +11,8 @@ const storedIds = JSON.parse(localStorage.getItem('selectedPlace')) || [];
 const storedPlaces = storedIds.map((id) => AVAILABLE_PLACES.find((place) => place.id === id));
 
 function App() {
-
-
-  const modal = useRef();
-  const selectedPlace = useRef();
+const selectedPlace = useRef();
+const [modalIsOpen, setModalIsOpen] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
   const [pickedPlaces, setPickedPlaces] = useState(storedPlaces);
 
@@ -31,12 +29,12 @@ function App() {
  
 
   function handleStartRemovePlace(id) {
-    modal.current.open();
+    setModalIsOpen(true);
     selectedPlace.current = id;
   }
 
   function handleStopRemovePlace() {
-    modal.current.close();
+    setModalIsOpen(false);
   }
 
   function handleSelectPlace(id) {
@@ -61,7 +59,7 @@ function App() {
     setPickedPlaces((prevPickedPlaces) =>
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
-    modal.current.close();
+    setModalIsOpen(false);
 
     const storedIds = JSON.parse(localStorage.getItem('selectedPlace')) || [];
   localStorage.setItem('selectedPlace', JSON.stringify(storedIds.filter((id) => id !== selectedPlace.current)));
@@ -69,7 +67,7 @@ function App() {
 
   return (
     <>
-      <Modal ref={modal}>
+      <Modal open={modalIsOpen}>
         <DeleteConfirmation
           onCancel={handleStopRemovePlace}
           onConfirm={handleRemovePlace}
